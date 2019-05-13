ab91b03cfd798499a45219e42559fe8f8f0253b2
Victor Garcia <vgarcialosada@gmail.com>
2019-05-13 19:12:27 +0200

    comit1
    commit

--- Assets/Scripts/SelectScene.cs
+++ Assets/Scripts/SelectScene.cs
@@ -10,33 +10,31 @@ public class SelectScene : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            selectScene();
+
+            selectScene(touch);
             } }
+            
 
-
-            public void selectScene()
-    {
+            public void selectScene(Touch t){ 
             Debug.Log(this.gameObject.name);
 
                 switch (this.gameObject.name)
             {
             case "AboutUsButton":
-                SceneManager.LoadScene("AboutUsButton");
+                SceneManager.LoadScene("6.AboutUsScene");
                 break;
-          
-       
                 case "RankingButton":
-                    SceneManager.LoadScene("RankingScene");
+                    SceneManager.LoadScene("9.RankingScene");
                     break;
             case "Back":
-                SceneManager.LoadScene("MainScreen");
+                SceneManager.LoadScene("4.MainScreen");
                 break;
 
             case "ProfileButton":
                 SceneManager.LoadScene("ProfileScreen");
                 break;
             case "PlayButton":
-                SceneManager.LoadScene("PlayScene");
+                SceneManager.LoadScene("7.PlayScene");
                 break;
 
         }

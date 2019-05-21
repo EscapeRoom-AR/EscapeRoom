891b64befc313b2b5545117f3eea98f3c9162c46
Alex <acuevasdev@gmail.com>
2019-05-21 20:19:11 +0200

    onClick

---
+++ Assets/Scripts/SelectScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectScene : MonoBehaviour
+{
+    public void changeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}

28d6124d03229a531ba8c83ae05871b162f6deb1
alex <5alexbouoliete@gmail.com>
2019-05-22 14:35:39 +0100

    authcontroller

---
+++ Assets/Controller/StartController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public AuthController authController;
+
+    void Start()
+    {
+        string nameScene = authController.IsAuthenticated() ? "Main" : "Login";
+        StartCoroutine(Timeout(nameScene));
+    }
+
+    IEnumerator Timeout(string nameScene)
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(nameScene);
+    }
+
+}

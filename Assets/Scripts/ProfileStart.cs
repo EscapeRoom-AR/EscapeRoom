a888a91810bc1516e2b22d919afabd51234f7fd5
alex <5alexbouoliete@gmail.com>
2019-05-23 21:33:13 +0100

    dynamic profile depending on logged in

---
+++ Assets/Scripts/ProfileStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ProfileStart : MonoBehaviour
+{
+    public AuthController authController;
+    public GameObject windowLoggedPrefab;
+    public GameObject windowUnLoggedPrefab;
+    public Canvas canvas;
+
+    void Start()
+    {
+        if (authController.IsAuthenticated())
+        {
+            GameObject window = Instantiate(windowLoggedPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            window.transform.SetParent(canvas.transform, false);
+        } else
+        {
+            GameObject window = Instantiate(windowUnLoggedPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            window.transform.SetParent(canvas.transform, false);
+        }
+            
+
+    }
+}

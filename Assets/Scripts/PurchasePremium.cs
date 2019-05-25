98acc40eaa28a4dea073603b138694f5b7b21e14
alex <5alexbouoliete@gmail.com>
2019-05-25 15:46:27 +0100

    iap configuration

---
+++ Assets/Scripts/PurchasePremium.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class PurchasePremium : MonoBehaviour
+{
+
+    public void BuyComplete(Product product)
+    {
+        print("Premium bought");
+    }
+
+    public void BuyFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        print("Premium buy error: " + failureReason);
+    }
+}

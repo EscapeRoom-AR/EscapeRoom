480d3bd165092dcbfd238bbe91c6a853e12c8aac
alex <5alexbouoliete@gmail.com>
2019-05-31 19:00:13 +0100

    more shit

--- Assets/Services/RESTService.cs
+++ Assets/Services/RESTService.cs
@@ -1,12 +1,14 @@
 ﻿using Model;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Threading.Tasks;
 
 namespace Services
 {
@@ -23,10 +25,11 @@ namespace Services
         private static string REGISTER = HOST + "/register?username={0}&email={1}&password={2}";
         private static string USER = HOST + "/user";
         private static string UPDATE_USER = USER + "?username={0}&email={1}&description={2}";
+        private static string UPDATE_USER_PHOTO = USER + "?photo={0}";
         private static string ROOMS = HOST + "/room";
         private static string ROOM = HOST + "/room";
         private static string RANKING = HOST + "/ranking";
-        private static string PHOTO = HOST + "/photo";
+        private static string PHOTO = "http://alexraspberry.ddns.net:8080/post.php";
 
         public void Login(User user,ResponseCallback<string> listener)
         {
@@ -53,9 +56,24 @@ namespace Services
             StartCoroutine(Request(String.Format(UPDATE_USER,user.Username,user.Email,user.Description), "PUT", listener));
         }
 
+        public void UpdateUserImage(string photoUrl, ResponseCallback<User> listener)
+        {
+            StartCoroutine(Request(String.Format(UPDATE_USER_PHOTO, photoUrl), "PUT", listener));
+        }
+
         public void PostPhoto(string photo, ResponseCallback<string> listener)
         {
-            StartCoroutine(Post3(PHOTO,photo, listener));
+            StartCoroutine(PostImage(photo,listener));
+        }
+
+        // Downloads a sprite given a url.
+        IEnumerator ImageCoroutine(string url, ImageCallBack callback)
+        {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+            while (!www.downloadHandler.isDone) { }
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture as Texture2D;
+            callback(Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
         }
 
         // Generic method for making a request to the web service, unfortunately
@@ -74,6 +92,45 @@ namespace Services
             callBack(apiResponse);
         }
 
+
+        IEnumerator PostImage(string base64, ResponseCallback<string> callBack)
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("photo", base64);
+            UnityWebRequest www = UnityWebRequest.Post(PHOTO, form);
+            www.SetRequestHeader("Authorization", PlayerPrefs.GetString("token"));
+            www.chunkedTransfer = false;
+            yield return www.SendWebRequest();
+            APIResponse<string> apiResponse;
+            print(www.downloadHandler.text);
+            print("hola?");
+            if (www.error != "")
+                apiResponse = new APIResponse<string>(0, www.error);
+            else
+                apiResponse = JsonUtility.FromJson<APIResponse<string>>(www.downloadHandler.text);
+            callBack(apiResponse);
+        }
+
+        /*
+        IEnumerator UglyPost<T>(string URI, string body, ResponseCallback<T> callBack)
+        {
+            UnityWebRequest www = new UnityWebRequest(URI, "POST");
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Authorization", PlayerPrefs.GetString("token"));
+            //string part1 = body.Substring(0, body.Length / 2);
+            //string part2 = body.Substring(body.Length / 2 + 1);
+            //www.SetRequestHeader("body1", part1);
+            //www.SetRequestHeader("body2", part2);
+            yield return www.SendWebRequest();
+            APIResponse<T> apiResponse;
+            print(www.downloadHandler.text);
+            if (www.isNetworkError || www.isHttpError)
+                apiResponse = new APIResponse<T>(0, www.error);
+            else
+                apiResponse = JsonUtility.FromJson<APIResponse<T>>(www.downloadHandler.text);
+            callBack(apiResponse);
+        }*/
+        /*
         IEnumerator Post<T>(string URI,string base64, ResponseCallback<T> callBack)
         {
             byte[] bodyRaw = Encoding.UTF8.GetBytes("{ 'photo': '" + base64 + "'}");
@@ -96,16 +153,6 @@ namespace Services
             else
                 apiResponse = JsonUtility.FromJson<APIResponse<T>>(www.downloadHandler.text);
             callBack(apiResponse);
-        }
-
-        // Downloads a sprite given a url.
-        IEnumerator ImageCoroutine(string url, ImageCallBack callback)
-        {
-            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
-            yield return www.SendWebRequest();
-            while (!www.downloadHandler.isDone) {  }
-            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture as Texture2D;
-            callback(Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f));
         }
 
         IEnumerator Post2<T>(string URI, string base64, ResponseCallback<T> callBack)
@@ -144,5 +191,67 @@ namespace Services
                 apiResponse = JsonUtility.FromJson<APIResponse<T>>(www.downloadHandler.text);
             callBack(apiResponse);
         }
+
+        async Task Post4Async(string URI, string base64)
+        {
+            print("starting post...");
+            HttpClient client = new HttpClient();
+            var values = new Dictionary<string, string>{ { "photo", base64 } };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PutAsync(URI, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            print(responseString + ",");
+        }
+
+        IEnumerator Upload()
+        {
+            var formData = Encoding.UTF8.GetBytes("{ 'photo': 'aaa'}");
+            UploadHandler uploader = new UploadHandlerRaw(formData);
+            uploader.contentType = "application/json";
+            DownloadHandler downloader = new DownloadHandlerBuffer();
+            UnityWebRequest www = new UnityWebRequest();
+            www.method = UnityWebRequest.kHttpVerbPOST;
+            www.uploadHandler = uploader;
+            www.downloadHandler = downloader;
+            www.chunkedTransfer = false;
+            www.url = PHOTO;
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                print("Response: " + www.downloadHandler.text);
+                print("Data: " + Encoding.UTF8.GetString(www.uploadHandler.data));
+            }
+        }
+
+        void Post6()
+        {
+            UnityWebRequest webRequest = new UnityWebRequest(PHOTO, "POST");
+
+            byte[] encodedPayload = new System.Text.UTF8Encoding().GetBytes("{ 'photo': 'aaa'}");
+            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(encodedPayload);
+            webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+            webRequest.SetRequestHeader("cache-control", "no-cache");
+            webRequest.chunkedTransfer = false;
+            UnityWebRequestAsyncOperation requestHandel = webRequest.SendWebRequest();
+            requestHandel.completed += delegate (AsyncOperation pOperation) {
+                print("Response: " + webRequest.downloadHandler.text);
+                print("Data: " + Encoding.UTF8.GetString(webRequest.uploadHandler.data));
+            };
+        } */
+
+
+
+
+
     }
 }

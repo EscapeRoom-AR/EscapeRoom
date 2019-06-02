32db6bf33cd3544fa7bf9bef7e3409df26a41b5d
alex <5alexbouoliete@gmail.com>
2019-06-02 20:28:14 +0100

    endscene

---
+++ Assets/Code/Scripts/UI/EndScene/EndSceneStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndSceneStart : MonoBehaviour
+{
+    public Text Score;
+    public Text Title;
+
+
+    void Start()
+    {
+        Title.text = Data.win ? "Congratulations!" : "Time's up!";
+        Score.text = "You scored " + Data.score + " points!";        
+    }
+}

046e2a23c2973bdcc1f8271a33e7e870e76010ba
alex <5alexbouoliete@gmail.com>
2019-06-02 18:44:36 +0100

    settings sound and tutorial

---
+++ Assets/Code/Scripts/UI/Settings/ToggleSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSound : MonoBehaviour
+{
+    public void OnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("sound", value ? 1 : 0);
+    }
+}
---
+++ Assets/Code/Scripts/UI/Settings/ToggleTutorial.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTutorial : MonoBehaviour
+{
+    public void OnValueChanged(bool value)
+    {
+        PlayerPrefs.SetInt("tutorial", value ? 1 : 0);
+    }
+}
